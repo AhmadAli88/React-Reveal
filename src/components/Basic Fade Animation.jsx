6ae5e3400c3ced7df7a42141855a10085411f4e4
Ahmad Ali <xevenahmad@gmail.com>
2024-11-16 23:59:47 +0500

    added more animate components

---
+++ src/components/Basic Fade Animation.jsx
@@ -0,0 +1,12 @@
+;
+import Fade from 'react-reveal/Fade';
+
+const FadeExample = () => (
+  <Fade>
+    <div style={{ textAlign: 'center', fontSize: '20px' }}>
+      This text fades in!
+    </div>
+  </Fade>
+);
+
+export default FadeExample;
---
+++ src/components/Zoom Animation.jsx
@@ -0,0 +1,12 @@
+
+import Zoom from 'react-reveal/Zoom';
+
+const ZoomExample = () => (
+  <Zoom>
+    <div style={{ textAlign: 'center', fontSize: '20px' }}>
+      Zoom into view!
+    </div>
+  </Zoom>
+);
+
+export default ZoomExample;
